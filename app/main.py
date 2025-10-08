8cf9e6e0f634e3727e30df3246b046c6f5818217
Palots <157062265+Palots@users.noreply.github.com>
2025-10-08 16:34:15 -0500

    Se añadieron cambios al endpoint  profileController

--- app/main.py
+++ app/main.py
@@ -1,7 +1,15 @@
 from fastapi import FastAPI
+from controllers import profileController
+from database import Base, engine
+
+# Crear las tablas en la base de datos (solo la primera vez)
+Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
+# Registrar el router del perfil
+app.include_router(profileController.router)
+
 @app.get("/")
-def read_root():
-    return {"Hello": "World"}
+def root():
+    return {"message": "API de perfiles funcionando correctamente 🚀"}
